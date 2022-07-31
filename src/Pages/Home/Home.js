7c169eab840d64ffa2c509b7e2a25402a3cfb942
abhaypsamrat <abhay.p.samrat@gmail.com>
2022-07-31 21:08:04 +0530

    add category

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import './Home.css'
+import { MenuItem, TextField } from "@mui/material";
+import "./Home.css";
+import Categories from "../../Data/Categories";
 
 const Home = () => {
   return (
-    <div className='content'>
+    <div className="content">
       <div className="setting">
-        <span style={{fontSize: 30}}>Quiz setting</span>
-      </div>
-      <img src='./quiz.svg' className='banner' alt='quiz img'/>
-    </div>
-  )
-}
+        <span style={{ fontSize: 30 }}>Quiz setting</span>
 
-export default Home
+        <div className="settings__select">
+          <TextField
+            style={{ marginBottom: 25 }}
+            label="Enter Your Name"
+            variant="outlined"
+          />
+          <TextField
+            select
+            label="Select Category"
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+          >
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+      </div>
+      <img src="./quiz.svg" className="banner" alt="quiz img" />
+    </div>
+  );
+};
+
+export default Home;
