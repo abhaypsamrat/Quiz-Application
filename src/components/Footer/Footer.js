f8e4ba4bfd0d26bd79cbc7d3de4018f85af64366
abhaypsamrat <abhay.p.samrat@gmail.com>
2022-11-26 18:15:10 +0530

    change footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <div className="footer" style={{ textAlign: "center", marginBottom: 10 }}>
       <hr />
-     <p> Made with ❤️ by{" "}
+      <p>QUIZHUB © 2022</p>
+      {/* <p> Made with ❤️ by{" "}
       <a
         href="https://apsamrat.netlify.app/"
         style={{ cursor: "pointer" }}
@@ -13,7 +14,7 @@ const Footer = () => {
       >
         <span>Samrat</span>
       </a>
-      </p>
+      </p> */}
     </div>
   );
 };
